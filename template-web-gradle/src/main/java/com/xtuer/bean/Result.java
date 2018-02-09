b2729b3a141430c48b7505bbfb02ac5d692166a0
Biao <biao.mac@icloud.com>
2018-02-09 19:47:04 +0800

    优化 Result

--- template-web-gradle/src/main/java/com/xtuer/bean/Result.java
+++ template-web-gradle/src/main/java/com/xtuer/bean/Result.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 /**
  * Http Ajax 请求返回时用作返回的对象，FastJson 自动转换为 Json 字符串返回给前端。
+ * 提供了多个变种的 ok() 和 fail() 方法简化创建 Result 对象。
  *
  * 虽然同一个请求在不同情况下返回的 Result 中的 data 类型可能不同，例如 Result<User> findUserByName(String name)，
  * 查询到用户时返回 Result 中 data 是 User 对象，查询不到用户时可返回 Result 中 data 是 String 对象，不过没关系，
@@ -16,26 +17,24 @@ import lombok.Setter;
 @Getter
 @Setter
 public final class Result<T> {
-    private boolean success;    // 成功时为 true，失败时为 false
-    private String  message;    // 成功或则失败时的描述信息
-    private Object  data;       // 成功或则失败时的更多详细数据，一般失败时不需要
-    private Integer statusCode; // 状态码，一般是当 success 为 true 或者 false 时不足够表达时可使用
+    private boolean success; // 成功时为 true，失败时为 false
+    private String  message; // 成功或则失败时的描述信息
+    private Object  data;    // 成功或则失败时的更多详细数据，一般失败时不需要
+    private Integer code;    // 状态码，一般是当 success 为 true 或者 false 时不足够表达时才使用，平时忽略即可
 
     public Result(boolean success, String message) {
         this(success, message, null);
     }
 
     public Result(boolean success, String message, Object data) {
-        this.success = success;
-        this.message = message;
-        this.data = data;
+        this(success, message, data, null);
     }
 
-    public Result(boolean success, String message, Object data, int statusCode) {
+    public Result(boolean success, String message, Object data, Integer code) {
         this.success = success;
         this.message = message;
         this.data = data;
-        this.statusCode = statusCode;
+        this.code = code;
     }
 
     public static <T> Result<T> ok() {
@@ -50,6 +49,10 @@ public final class Result<T> {
         return new Result<>(true, message, data);
     }
 
+    public static <T> Result<T> ok(String message, Object data, Integer code) {
+        return new Result<>(true, message, data, code);
+    }
+
     public static <T> Result<T> fail() {
         return new Result<>(false, "fail");
     }
@@ -60,6 +63,10 @@ public final class Result<T> {
 
     public static <T> Result<T> fail(String message, Object data) {
         return new Result<>(false, message, data);
+    }
+
+    public static <T> Result<T> fail(String message, Object data, Integer code) {
+        return new Result<>(false, message, data, code);
     }
 
     /**
@@ -78,14 +85,17 @@ public final class Result<T> {
 
     // 测试
     public static void main(String[] args) {
-        // JSON
+        // Result
         Result<User> r1 = Result.ok();
         Result<User> r2 = Result.ok(new User("Alice", "Passw0rd"));
         Result<User> r3 = Result.ok("Yes", new Demo(123456L, "Physics"));
+        Result<User> r4 = Result.ok("Yes", new Demo(123456L, "Physics"), 1024);
 
+        // JSON
         System.out.println(JSON.toJSONString(r1));
         System.out.println(JSON.toJSONString(r2));
         System.out.println(JSON.toJSONString(r3));
+        System.out.println(JSON.toJSONString(r4));
 
         System.out.println(r3.getData());
 
