c62a368ccbe515448ac64bf32006612e5210fcc3
Biao <biao.mac@icloud.com>
2019-07-18 15:31:19 +0800

    wx

--- template-web-gradle/src/main/java/edu/config/AppConfig.java
+++ template-web-gradle/src/main/java/edu/config/AppConfig.java
@@ -3,12 +3,12 @@ package edu.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 应用的配置，系统中使用的配置都使用一个变量保存，方便访问以及修改
  */
-@Service
+@Configuration
 @Getter
 @Setter
 public class AppConfig {
