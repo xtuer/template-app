81c0ea8b6fc89b536f1377c91651b389172a5fdb
Biao <biao.mac@icloud.com>
2019-08-20 10:02:21 +0800

    vue

--- template-vue-app/vue.config.js
+++ template-vue-app/vue.config.js
@@ -1,9 +1,12 @@
 process.env.VUE_APP_VERSION = new Date().getTime();
 
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 module.exports = {
     devServer: {
         port: 8888,
-        proxy: 'http://localhost:8080'
+        proxy: 'http://localhost:8080',
+        disableHostCheck: true,
     },
 
     // 多页的页面
@@ -24,5 +27,17 @@ module.exports = {
                 `
             }
         }
-    }
+    },
+    productionSourceMap: false, // 不生成 map 文件
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            return {
+                plugins: [new CompressionWebpackPlugin({
+                    test: /\.(js|css)(\?.*)?$/i, // 需要压缩的文件正则
+                    threshold: 10240,            // 文件大小大于这个值时启用压缩 (10K)
+                    deleteOriginalAssets: false, // 压缩后保留原文件
+                })]
+            };
+        }
+    },
 };
