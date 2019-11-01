3ca11a195901c7daab8c9aeb7a2091b0a3a18b53
Biao <biao.mac@icloud.com>
2019-11-01 19:39:38 +0800

    日期转换支持 yyyy-MM-ddTHH:mm:ss.SZ 格式

--- template-web-gradle/src/main/java/com/edu/training/converter/DateConverter.java
+++ template-web-gradle/src/main/java/com/edu/training/converter/DateConverter.java
@@ -1,6 +1,7 @@
 package com.edu.training.converter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.core.convert.converter.Converter;
 
@@ -13,8 +14,12 @@ import java.util.Date;
  */
 @Slf4j
 public class DateConverter implements Converter<String, Date> {
+    private static final SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+    private static final SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat format3 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
+
     /**
-     * 把日期字符串转换为 Date 对象，接收两种日期格式: yyyy-MM-dd 或者 yyyy-MM-dd HH:mm:ss。
+     * 把日期字符串转换为 Date 对象，接收三种日期格式: yyyy-MM-dd、yyyy-MM-dd HH:mm:ss 或者 yyyy-MM-ddTHH:mm:ss.SZ
      * 如果日期的格式不对，则返回 null。
      *
      * @param source 字符串格式的日期
@@ -22,8 +27,18 @@ public class DateConverter implements Converter<String, Date> {
      */
     @Override
     public Date convert(String source) {
-        String pattern = source.length()==10 ? "yyyy-MM-dd" : "yyyy-MM-dd HH:mm:ss";
-        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        if (StringUtils.isBlank(source)) {
+            return null;
+        }
+
+        SimpleDateFormat format = null;
+
+        switch (source.length()) {
+            case 10: format = format1; break;
+            case 19: format = format2; break;
+            case 24: format = format3; break;
+            default: return null;
+        }
 
         try {
             return format.parse(source);
