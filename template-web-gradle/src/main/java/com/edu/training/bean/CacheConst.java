376b94880961fe8dddaeb541d03603b7a4719c3e
Biao <biao.mac@icloud.com>
2019-11-02 19:32:08 +0800

    O_O

--- template-web-gradle/src/main/java/com/edu/training/bean/CacheConst.java
+++ template-web-gradle/src/main/java/com/edu/training/bean/CacheConst.java
@@ -4,7 +4,7 @@ package com.edu.training.bean;
  * 缓存使用的键和缓存的名字
  */
 public interface CacheConst {
-    String CACHE = "training."; // 默认的缓存对象
+    String CACHE = "training:"; // 默认的缓存对象
 
     String KEY_USER = "'user.' + #userId";
     String KEY_ORG  = "'org.' + #host";
