03c24825069bc31412eb18bdbee0955ced65e10e
Biao <biao.mac@icloud.com>
2019-11-08 14:44:36 +0800

    avatar dir use date

--- template-web-gradle/src/main/java/com/edu/training/service/UserService.java
+++ template-web-gradle/src/main/java/com/edu/training/service/UserService.java
@@ -226,7 +226,7 @@ public class UserService extends BaseService {
         // 3. 删除缓存用户
         // 4. 返回 avatar 的 url
 
-        avatar = repoFileService.moveTempFileToRepo(avatar, "avatar");
+        avatar = repoFileService.moveTempFileToRepo(avatar);
 
         if (avatar != null) {
             userMapper.updateUserAvatar(userId, avatar);
