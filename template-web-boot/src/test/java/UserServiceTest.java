6e420f6e1e629c23ae390f6717997e427ea08fb0
Biao <biao.mac@icloud.com>
2020-01-14 17:33:21 +0800

    O-O

---
+++ template-web-boot/src/test/java/UserServiceTest.java
@@ -0,0 +1,19 @@
+import com.xtuer.Application;
+import com.xtuer.service.UserService;
+import com.xtuer.util.Utils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles({"mac"}) // 指定测试的 active profile (dev, default)
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void findUser() {
+        Utils.dump(userService.findUser("admin", 1));
+    }
+}
