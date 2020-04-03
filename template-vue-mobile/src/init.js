dadf9335ee59df83cd543c713621e4c846b7bf27
Biao <biao.mac@icloud.com>
2020-04-03 11:55:47 +0800

    Noop

--- template-vue-mobile/src/init.js
+++ template-vue-mobile/src/init.js
@@ -7,8 +7,26 @@ import Vue from 'vue';
 import { Button, Toast, Field, CellGroup, Uploader } from 'vant';
 import '@/../public/static-m/lib/axios.rest';
 
+import '@/../public/static-m/js/urls';
+import '@/../public/static-m/js/utils';
+import '@/../public/static-m/js/constants';
+import '@/../public/static-m/lib/axios.rest';
+
+import filters from '@/../public/static-m/js/filters';
+import methods from '@/../public/static-m/js/methods';
+
 Vue.use(Button);
 Vue.use(Toast);
 Vue.use(Field);
 Vue.use(CellGroup);
 Vue.use(Uploader);
+
+// [3] 注册全局过滤器
+Object.keys(filters).forEach(key => {
+    Vue.filter(key, filters[key]);
+});
+
+// [4] 注册 Vue 的原型函数
+Object.keys(methods).forEach(key => {
+    Vue.prototype[key] = methods[key];
+});
