78e3d752a273dc3c2d0a4242d9f18ef26d13a43d
Biao <biao.mac@icloud.com>
2020-06-04 17:23:27 +0800

    角色增加中文名

--- template-web-boot/src/main/java/com/xtuer/bean/Role.java
+++ template-web-boot/src/main/java/com/xtuer/bean/Role.java
@@ -4,10 +4,21 @@ package com.xtuer.bean;
  * 用户角色
  */
 public enum Role {
-    ROLE_ADMIN_SYSTEM, // 系统管理员
-    ROLE_ADMIN_ORG,    // 机构管理员
-    ROLE_FORM_TEACHER, // 班主任
-    ROLE_TEACHER,      // 老师
-    ROLE_STUDENT,      // 学生
-    ROLE_USER,         // 普通用户
+    ROLE_ADMIN_SYSTEM("系统管理员"),
+    ROLE_ADMIN_ORG("机构管理员"),
+    ROLE_FORM_TEACHER("班主任"),
+    ROLE_TEACHER("老师"),
+    ROLE_STUDENT("学生"),
+    ROLE_USER("普通用户");
+
+    // 角色的中文名字，方便记忆 (JSON 序列化的时候不会输出)
+    private final String label;
+
+    Role(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
 }
