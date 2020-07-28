1aa301acf4a7c9737537f03488640ef7973aae9c
Biao <huangbiao@shindata.com>
2020-07-28 11:47:42 +0800

    解决垂直滚动条出现时占据空间显示水平滚动条

--- template-vue-pc/src/pages/page1/App.vue
+++ template-vue-pc/src/pages/page1/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style lang="scss">
-body, html, #app {
+html, body, #app {
     min-width : 960px;
     width     : 100%;
     height    : 100%;
@@ -15,5 +15,11 @@ body, html, #app {
     font-family: Helvetica, "Microsoft YaHei", Arial, sans-serif;
     -webkit-font-smoothing : antialiased;
     -moz-osx-font-smoothing: grayscale;
+}
+
+/* 解决垂直滚动条出现时占据空间显示水平滚动条 */
+:root {
+  overflow-y: auto;
+  overflow-x: hidden;
 }
 </style>
