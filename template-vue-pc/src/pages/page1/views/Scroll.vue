b8cc1ffbf2f23f9335a54ce7a1d34ce4df4ae78b
Biao <huangbiao@shindata.com>
2020-08-09 15:27:56 +0800

    测试 leader line

--- template-vue-pc/src/pages/page1/views/Scroll.vue
+++ template-vue-pc/src/pages/page1/views/Scroll.vue
@@ -3,6 +3,11 @@
     <div class="scroll-demo">
         <Affix :offset-top="20"><FileUpload/></Affix>
 
+        <!-- Leader Lines -->
+        <div class="connections">
+            <div v-for="item in items" :id="item" :key="item" class="item">{{ item }}</div>
+        </div>
+
         <p>1</p>
         <p>2</p>
         <p>3</p>
@@ -14,11 +19,26 @@ import FileUpload from '@/components/FileUpload.vue';
 
 export default {
     components: { FileUpload },
+    data() {
+        return {
+            items: ['One', 'Two', 'Three', 'Four', 'Five', 'Six'],
+            lines: [],
+        };
+    },
     mounted() {
         window.addEventListener('scroll', this.fixAffix, true);
+
+        Utils.loadJs('/static-p/lib/leader-line.min.js').then(() => {
+            this.buildLines();
+        });
     },
     beforeDestroy() {
         window.removeEventListener('scroll', this.fixAffix, true);
+
+        // 销毁连线
+        this.lines.forEach(line => {
+            line.remove();
+        });
     },
     methods: {
         // 解决 Affix 不生效
@@ -29,6 +49,15 @@ export default {
                 window.dispatchEvent(event);
             } else if (document.createEventObject) {
                 window.fireEvent('onresize');
+            }
+        },
+        // 创建连线
+        buildLines() {
+            for (let i = 0; i < this.items.length-1; i++) {
+                var startElement = document.getElementById(this.items[i]);
+                var endElement   = document.getElementById(this.items[i+1]);
+
+                this.lines.push(new LeaderLine(startElement, endElement, { color: '#aaa', size: 2 }));
             }
         },
     }
@@ -44,6 +73,21 @@ export default {
         margin-top: 20px;
         background-image: linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%);
         // width: 2000px;
+    }
+
+    .connections {
+        padding: 20px;
+        border: 1px solid #eee;
+        margin-top: 20px;
+
+        .item {
+            display: inline-block;
+            border: 1px solid #aaa;
+            padding: 20px;
+            margin: 20px;
+            width: 150px;
+            text-align: center;
+        }
     }
 }
 </style>
