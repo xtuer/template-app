09250b5a7fd93e3023f6ae40d853e37a118bf5c5
Biao <huangbiao@shindata.com>
2021-01-05 10:58:57 +0800

    application exception

--- template-web-boot/src/main/java/com/xtuer/exception/ApplicationException.java
+++ template-web-boot/src/main/java/com/xtuer/exception/ApplicationException.java
@@ -1,22 +1,36 @@
 package com.xtuer.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * 定义应用程序级别统一的异常，能够指定异常显示的页面，即 error page file。
  * 默认的错误页面是 error.html，异常还能传入 errorPageFile 指定自己的错误页面。
  */
+@Getter
+@Setter
 public class ApplicationException extends RuntimeException {
-    private String errorPageFile = null; // 错误页面的模版文件
+    /**
+     * 错误代码: 应用内部传递异常使用
+     */
+    private int code;
+
+    /**
+     * 错误页面的模版文件: 给错误页面使用
+     */
+    private String errorPageFile;
 
     public ApplicationException(String message) {
         this(message, null);
     }
 
+    public ApplicationException(String message, int code) {
+        this(message, null);
+        this.code = code;
+    }
+
     public ApplicationException(String message, String errorPageFile) {
         super(message);
         this.errorPageFile = errorPageFile;
-    }
-
-    public String getErrorPageFile() {
-        return errorPageFile;
     }
 }
