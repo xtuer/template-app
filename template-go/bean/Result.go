5f296af467656428f4fa55014870251d3b000f4e
huangbiao <huangbiao@shindata.com>
2022-12-11 12:18:26 +0800

    opt: 优化响应函数名

--- template-go/bean/Result.go
+++ template-go/bean/Result.go
@@ -15,24 +15,24 @@ type Result struct {
 	Data    any    `json:"data,omitempty"` // 请求的 payload
 }
 
-// FailWithMessage 设置处理失败的响应数据。
-func FailWithMessage(c *gin.Context, reason any) {
+// FailResponseWithMessage 设置处理失败的响应数据。
+func FailResponseWithMessage(c *gin.Context, reason any) {
 	c.AbortWithStatusJSON(http.StatusOK, Result{
 		Success: false,
 		Msg:     fmt.Sprintf("%v", reason),
 	})
 }
 
-// OkWithData 设置处理成功的响应数据。
-func OkWithData(c *gin.Context, data any) {
+// OkResponseWithData 设置处理成功的响应数据。
+func OkResponseWithData(c *gin.Context, data any) {
 	c.IndentedJSON(http.StatusOK, Result{
 		Success: true,
 		Data:    data,
 	})
 }
 
-// OkWithMessage 设置处理成功的响应数据。
-func OkWithMessage(c *gin.Context, msg string) {
+// OkResponseWithMessage 设置处理成功的响应数据。
+func OkResponseWithMessage(c *gin.Context, msg string) {
 	c.IndentedJSON(http.StatusOK, Result{
 		Success: true,
 		Msg:     msg,
--- template-go/controller/ExecuteController.go
+++ template-go/controller/ExecuteController.go
@@ -49,7 +49,7 @@ func (o *ExecuteController) ExecuteCmd() gin.HandlerFunc {
 
 		// [2] 参数获取: 把请求体中的数据绑定到 job。
 		if err := c.ShouldBindJSON(job); err != nil {
-			bean.FailWithMessage(c, err)
+			bean.FailResponseWithMessage(c, err)
 			return
 		}
 
@@ -58,13 +58,13 @@ func (o *ExecuteController) ExecuteCmd() gin.HandlerFunc {
 
 		// [3] 参数校验: job 的 Cmd 字段不能为空。
 		if err := o.executeService.ValidateCmdJob(job); err != nil {
-			bean.FailWithMessage(c, err)
+			bean.FailResponseWithMessage(c, err)
 			return
 		}
 
 		// [4] 执行 Job 的命令。
 		o.executeService.ExecuteJob(job)
-		bean.OkWithData(c, job)
+		bean.OkResponseWithData(c, job)
 	}
 }
 
@@ -91,7 +91,7 @@ func (o *ExecuteController) ExecuteScript() gin.HandlerFunc {
 
 		// [2] 参数获取: 把请求体中的数据绑定到 job。
 		if err := c.ShouldBindJSON(job); err != nil {
-			bean.FailWithMessage(c, err)
+			bean.FailResponseWithMessage(c, err)
 			return
 		}
 
@@ -102,13 +102,13 @@ func (o *ExecuteController) ExecuteScript() gin.HandlerFunc {
 
 		// [3] 参数校验: job 的 Cmd 字段不能为空。
 		if err := o.executeService.ValidateScriptJob(job); err != nil {
-			bean.FailWithMessage(c, err)
+			bean.FailResponseWithMessage(c, err)
 			return
 		}
 
 		// [4] 执行 Job 的脚本。
 		o.executeService.ExecuteJob(job)
-		bean.OkWithData(c, job)
+		bean.OkResponseWithData(c, job)
 	}
 }
 
@@ -125,9 +125,9 @@ func (o *ExecuteController) FindJobById() gin.HandlerFunc {
 		job := o.executeService.FindJobById(jobId)
 
 		if job == nil {
-			bean.FailWithMessage(c, fmt.Sprintf("Job not found, jobId: %s", jobId))
+			bean.FailResponseWithMessage(c, fmt.Sprintf("Job not found, jobId: %s", jobId))
 		} else {
-			bean.OkWithData(c, job)
+			bean.OkResponseWithData(c, job)
 		}
 	}
 }
